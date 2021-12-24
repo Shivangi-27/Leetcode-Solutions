cd55f681554f34e3a74cb0011102c8beb13ee6b0
Shivangi-Mohanty <shivangimohanty2001@gmail.com>
2021-12-24 21:43:44 +0530

    Time: 4 ms (94.25%), Space: 15.2 MB (39.21%) - LeetHub

---
+++ shuffle-string/shuffle-string.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string restoreString(string s, vector<int>& indices) {
+
+        string temp=s;
+        for(int i=0 ; i<indices.size() ; i++)
+        {
+             temp[indices[i]] = s[i];
+        }
+        
+        return temp;
+    }
+};
