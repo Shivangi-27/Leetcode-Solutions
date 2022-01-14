e32aef420fbe73bc207656bb2d1ae5edcad9887b
Shivangi-Mohanty <shivangimohanty2001@gmail.com>
2022-01-14 23:39:16 +0530

    Time: 84 ms (51.70%), Space: 46.6 MB (45.31%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.cpp
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -3,17 +3,12 @@ public:
     bool containsDuplicate(vector<int>& nums) {
         sort(nums.begin(),nums.end());
         
-        int c=0;
         for(int i=0 ; i<nums.size()-1 ; i++)
         {
             if(nums[i]==nums[i+1])
-            c++;
+            return true;
         }
         
-        if(c!=0)
-        return true;
-        
-        else
         return false;
     }
 };
