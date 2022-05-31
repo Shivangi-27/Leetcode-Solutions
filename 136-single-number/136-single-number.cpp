fe89d14e82ffbcf3d66088fb06e621040bbdd53c
Shivangi-Mohanty <shivangimohanty2001@gmail.com>
2022-05-31 14:41:48 +0530

    Time: 27 ms (54.87%), Space: 20.2 MB (7.70%) - LeetHub

--- 136-single-number/136-single-number.cpp
+++ 136-single-number/136-single-number.cpp
@@ -3,15 +3,14 @@ public:
     int singleNumber(vector<int>& nums) {
         
         unordered_map<int,int> map;
-        int ans;
         
         for(int i=0 ; i<nums.size() ; i++)
         map[nums[i]]++;
         
         for(auto it=map.begin() ; it!=map.end() ; it++)
         if(it->second == 1)
-        ans = it->first;                
+        return it->first;                
                 
-        return ans;
+        return 0;
     }
 };
