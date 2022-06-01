868dce63da2af337f3b7d58d446b6aecdd8c08dc
Shivangi-Mohanty <shivangimohanty2001@gmail.com>
2022-06-02 00:56:19 +0530

    Time: 9 ms (48.54%), Space: 10.3 MB (54.45%) - LeetHub

--- 349-intersection-of-two-arrays/349-intersection-of-two-arrays.cpp
+++ 349-intersection-of-two-arrays/349-intersection-of-two-arrays.cpp
@@ -2,21 +2,40 @@ class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
         
-        sort(nums1.begin() , nums1.end());
-        sort(nums2.begin() , nums2.end());
+//         sort(nums1.begin() , nums1.end());
+//         sort(nums2.begin() , nums2.end());
+//         vector<int> res;
+        
+//         for(int i=0 ; i<nums1.size() ; i++)
+//         {
+//             for(int j=0 ; j<nums2.size() ; j++)
+//             {
+//                 if(nums1[i]==nums2[j])
+//                 res.push_back(nums1[i]);
+                
+//             }
+//         }
+        
+//         res.erase(unique(res.begin() , res.end()) , res.end());
+        
+//         return res;
+        
+        unordered_set<int> set;
         vector<int> res;
         
         for(int i=0 ; i<nums1.size() ; i++)
+        set.insert(nums1[i]);
+        
+        for(int i=0 ; i<nums2.size() ; i++)
         {
-            for(int j=0 ; j<nums2.size() ; j++)
+            if(set.find(nums2[i]) != set.end())
             {
-                if(nums1[i]==nums2[j])
-                res.push_back(nums1[i]);
-                
+                res.push_back(nums2[i]);
+                set.erase(nums2[i]);
             }
         }
         
-        res.erase(unique(res.begin() , res.end()) , res.end());
+        
         
         return res;
     }
