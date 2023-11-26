07b1de6086ed8447cc8643513d47817171d9b513
Shivangi-Mohanty <shivangimohanty2001@gmail.com>
2023-11-26 20:25:31 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (7.33%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int reverse(int x) {
+
+        int rem, ans=0;
+
+        while(x!=0)
+        {
+            rem = x%10;
+
+            if(ans < INT_MIN/10 || ans > INT_MAX/10)
+            return 0;
+            
+            ans = ans*10 + rem;
+            x/=10;
+        }
+        
+        return ans;
+    }
+};
